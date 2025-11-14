58c930778244763be3d359ffd6f82e5a3ee352f4
Surendran <surendrans3232@gmail.com>
2025-11-14 15:31:11 +0530

    Update urls.py

--- animehubchennal/urls.py
+++ animehubchennal/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("animes/", AnimeAPIView.as_view()),
     path("seasons/", SeasonAPIView.as_view()),
     path("episodes/", EpisodeAPIView.as_view()),
-    path("home/",HelloView.as_view()),
+    path("hello/",HelloView.as_view()),
 ]
