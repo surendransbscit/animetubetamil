458cf6da1eda53d47bcf680c3457adf5697089c6
Surendran <surendrans3232@gmail.com>
2025-11-14 15:35:05 +0530

    Update settings.py

--- animehub/settings.py
+++ animehub/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'animehubchennal.apps.AnimehubchennalConfig',
     'rest_framework',
     'animehubchennal',
     'corsheaders',
