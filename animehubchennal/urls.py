c240dd214c30076e2d9b6cf708941551e8f3b639
Surendran <surendrans3232@gmail.com>
2025-11-14 15:29:14 +0530

    Add HelloView to URL patterns

--- animehubchennal/urls.py
+++ animehubchennal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ChannelAPIView, AnimeAPIView, SeasonAPIView, EpisodeAPIView
+from .views import ChannelAPIView, AnimeAPIView, SeasonAPIView, EpisodeAPIView,HelloView
 
 urlpatterns = [
     path("channels/", ChannelAPIView.as_view()),
     path("animes/", AnimeAPIView.as_view()),
     path("seasons/", SeasonAPIView.as_view()),
     path("episodes/", EpisodeAPIView.as_view()),
+    path("home/",HelloView.as_view()),
 ]
